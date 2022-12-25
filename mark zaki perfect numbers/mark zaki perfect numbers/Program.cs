82c37fded353d0a58f3c2d036f54484e2127e80e
mark zaki <120737045+markzaki77@users.noreply.github.com>
2022-12-25 16:50:05 +0200

    Add files via upload

---
+++ mark zaki perfect numbers/mark zaki perfect numbers/Program.cs
@@ -0,0 +1,36 @@
+﻿
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Enter the first num:");
+        int first_num = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Enter the second num:");
+        int second_num = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("The perfect numbers between {0} and {1} are:", first_num, second_num);
+        for (int i = first_num; i <= second_num; i++)
+        {
+            if (perfect_tester(i))
+            {
+                Console.Write(i + "\n");
+            }
+        }
+
+
+        static bool perfect_tester(int number)
+        {
+            int p = 0;
+            for (int i = 1; i < number; i++)
+            {
+                if (number % i == 0)
+                {
+                    p += i;
+                }
+            }
+
+            return p == number;
+        }
+    }
+}
